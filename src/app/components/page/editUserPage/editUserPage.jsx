2f7fe5885eecb262054fc370a22c3112484dc6c8
Anton Nosov <nosov.anton1994@gmail.com>
2022-10-22 14:11:12 +0300

    Auth_p2

--- src/app/components/page/editUserPage/editUserPage.jsx
+++ src/app/components/page/editUserPage/editUserPage.jsx
@@ -1,99 +1,61 @@
 import React, { useEffect, useState } from 'react'
 import { useHistory, useParams } from 'react-router-dom'
 import { validator } from '../../../utils/validator'
-import api from '../../../api'
 import TextField from '../../common/form/textField'
 import SelectField from '../../common/form/selectField'
 import RadioField from '../../common/form/radioField'
 import MultiSelectField from '../../common/form/multiSelectField'
 import BackHistoryButton from '../../common/backButton'
+import { useProfessions } from '../../../hooks/useProfession'
+import { useQualities } from '../../../hooks/useQualities'
+import { useUser } from '../../../hooks/useUsers'
+import { useAuth } from '../../../hooks/useAuth'
 
 const EditUserPage = () => {
     const { userId } = useParams()
     const history = useHistory()
-    const [isLoading, setIsLoading] = useState(false)
-    const [data, setData] = useState({
-        name: '',
-        email: '',
-        profession: '',
-        sex: 'male',
-        qualities: []
-    })
-    const [professions, setProfession] = useState([])
-    const [qualities, setQualities] = useState([])
+    const [data, setData] = useState()
+
+    const { getUserById } = useUser()
+    const user = getUserById(userId)
+    const { isLoading: qualitiesLoading, qualities } = useQualities()
+    const qualitiesList = qualities.map((q) => ({
+        label: q.name,
+        value: q._id
+    }))
+    const { isLoading: professionsLoading, professions } = useProfessions()
+    const professionsList = professions.map((p) => ({
+        label: p.name,
+        value: p._id
+    }))
+    const { updateUserData } = useAuth()
+
     const [errors, setErrors] = useState({})
-    const getProfessionById = (id) => {
-        for (const prof of professions) {
-            if (prof.value === id) {
-                return { _id: prof.value, name: prof.label }
-            }
-        }
-    }
-    const getQualities = (elements) => {
-        const qualitiesArray = []
-        for (const elem of elements) {
-            for (const quality in qualities) {
-                if (elem.value === qualities[quality].value) {
-                    qualitiesArray.push({
-                        _id: qualities[quality].value,
-                        name: qualities[quality].label,
-                        color: qualities[quality].color
-                    })
-                }
-            }
-        }
-        return qualitiesArray
-    }
-    const handleSubmit = (e) => {
+
+    const handleSubmit = async (e) => {
         e.preventDefault()
+        history.push(`/users/${data._id}`)
         const isValid = validate()
         if (!isValid) return
-        const { profession, qualities } = data
-        api.users
-            .update(userId, {
-                ...data,
-                profession: getProfessionById(profession),
-                qualities: getQualities(qualities)
-            })
-            .then((data) => history.push(`/users/${data._id}`))
-        console.log({
+        const newData = {
             ...data,
-            profession: getProfessionById(profession),
-            qualities: getQualities(qualities)
-        })
-    }
-    const transformData = (data) => {
-        return data.map((qual) => ({ label: qual.name, value: qual._id }))
+            qualities: data.qualities.map((q) => q.value)
+        }
+        try {
+            await updateUserData(newData)
+            history.push(`/users/${data._id}`)
+        } catch (error) {
+            setErrors(error)
+        }
     }
     useEffect(() => {
-        setIsLoading(true)
-        api.users.getById(userId).then(({ profession, qualities, ...data }) =>
-            setData((prevState) => ({
-                ...prevState,
-                ...data,
-                qualities: transformData(qualities),
-                profession: profession._id
-            }))
+        const userQualities = user.qualities.map((userQuality) =>
+            qualitiesList.find((q) => q.value === userQuality)
         )
-        api.professions.fetchAll().then((data) => {
-            const professionsList = Object.keys(data).map((professionName) => ({
-                label: data[professionName].name,
-                value: data[professionName]._id
-            }))
-            setProfession(professionsList)
-        })
-        api.qualities.fetchAll().then((data) => {
-            const qualitiesList = Object.keys(data).map((optionName) => ({
-                value: data[optionName]._id,
-                label: data[optionName].name,
-                color: data[optionName].color
-            }))
-            setQualities(qualitiesList)
-        })
-    }, [])
-    useEffect(() => {
-        if (data._id) setIsLoading(false)
-    }, [data])
+        if (user && !professionsLoading && !qualitiesLoading) {
+            setData({ ...user, qualities: userQualities })
+        }
+    }, [user, professionsLoading, qualitiesLoading])
 
     const validatorConfig = {
         email: {
@@ -130,7 +92,7 @@ const EditUserPage = () => {
             <BackHistoryButton />
             <div className="row">
                 <div className="col-md-6 offset-md-3 shadow p-4">
-                    {!isLoading && Object.keys(professions).length > 0 ? (
+                    {data && (
                         <form onSubmit={handleSubmit}>
                             <TextField
                                 label="Имя"
@@ -149,7 +111,7 @@ const EditUserPage = () => {
                             <SelectField
                                 label="Выбери свою профессию"
                                 defaultOption="Choose..."
-                                options={professions}
+                                options={professionsList}
                                 name="profession"
                                 onChange={handleChange}
                                 value={data.profession}
@@ -168,7 +130,7 @@ const EditUserPage = () => {
                             />
                             <MultiSelectField
                                 defaultValue={data.qualities}
-                                options={qualities}
+                                options={qualitiesList}
                                 onChange={handleChange}
                                 name="qualities"
                                 label="Выберите ваши качества"
@@ -181,8 +143,6 @@ const EditUserPage = () => {
                                 Обновить
                             </button>
                         </form>
-                    ) : (
-                        'Loading...'
                     )}
                 </div>
             </div>
--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -3,9 +3,17 @@ import PropTypes from 'prop-types'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 import userService from '../services/user.service'
-import { setTokens } from '../services/localStorage.service'
+import localStorageService, {
+    setTokens
+} from '../services/localStorage.service'
+import { useHistory } from 'react-router-dom'
 
-const httpAuth = axios.create()
+export const httpAuth = axios.create({
+    baseURL: 'https://identitytoolkit.googleapis.com/v1/',
+    params: {
+        key: process.env.REACT_APP_FIREBASE_KEY
+    }
+})
 const AuthContext = React.createContext()
 
 export const useAuth = () => {
@@ -13,20 +21,40 @@ export const useAuth = () => {
 }
 
 const AuthProvider = ({ children }) => {
-    const [currentUser, setUser] = useState({})
+    const [currentUser, setUser] = useState()
     const [error, setError] = useState(null)
+    const [isLoading, setLoading] = useState(true)
+    const history = useHistory()
 
+    function logOut() {
+        localStorageService.removeAuthData()
+        setUser(null)
+        history.push('/')
+    }
+    function randomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
     async function signUp({ email, password, ...rest }) {
-        const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_KEY}`
         try {
-            const { data } = await httpAuth.post(url, {
+            const { data } = await httpAuth.post(`accounts:signUp`, {
                 email,
                 password,
                 returnSecureToken: true
             })
             setTokens(data)
 
-            await createUser({ _id: data.localId, email, ...rest })
+            await createUser({
+                _id: data.localId,
+                email,
+                rate: randomInt(1, 5),
+                completedMeetings: randomInt(0, 200),
+                image: `https://avatars.dicebear.com/api/avataaars/${(
+                    Math.random() + 1
+                )
+                    .toString(36)
+                    .substring(7)}.svg`,
+                ...rest
+            })
         } catch (error) {
             errorCatcher(error)
             const { code, message } = error.response.data.error
@@ -41,47 +69,71 @@ const AuthProvider = ({ children }) => {
             throw new Error()
         }
     }
-    async function signIn({ email, password, ...rest }) {
-        const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_FIREBASE_KEY}`
+    async function logIn({ email, password }) {
         try {
-            const { data } = await httpAuth.post(url, {
-                email,
-                password,
-                returnSecureToken: true
-            })
+            const { data } = await httpAuth.post(
+                `accounts:signInWithPassword`,
+                {
+                    email,
+                    password,
+                    returnSecureToken: true
+                }
+            )
             setTokens(data)
-
-            await createUser({ _id: data.localId, email, ...rest })
+            await getUserData()
         } catch (error) {
             errorCatcher(error)
             const { code, message } = error.response.data.error
             if (code === 400) {
-                if (message === 'EMAIL_NOT_FOUND') {
-                    const errorObject = {
-                        email: 'Пользователя с таким Email не существует'
-                    }
-                    throw errorObject
-                }
-                if (message === 'INVALID_PASSWORD') {
-                    const errorObject = {
-                        password: 'Неверный пароль'
-                    }
-                    throw errorObject
+                switch (message) {
+                    case 'EMAIL_NOT_FOUND':
+                        throw new Error('Email или пароль введены некорректно')
+
+                    default:
+                        throw new Error(
+                            'Слишком много попыток входа. Попробуйте позднее'
+                        )
                 }
             }
-            throw new Error()
         }
     }
     async function createUser(data) {
         try {
-            const { content } = userService.create(data)
+            const { content } = await userService.create(data)
             setUser(content)
-        } catch (error) {}
+        } catch (error) {
+            errorCatcher(error)
+        }
     }
     function errorCatcher(error) {
         const { message } = error.response.data
         setError(message)
     }
+    async function getUserData() {
+        try {
+            const { content } = await userService.getCurrentUser()
+            setUser(content)
+        } catch (error) {
+            errorCatcher(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+    async function updateUserData(data) {
+        try {
+            const { content } = await userService.updateCurrentUser(data)
+            setUser(content)
+        } catch (error) {
+            errorCatcher(error)
+        }
+    }
+    useEffect(() => {
+        if (localStorageService.getAccessToken()) {
+            getUserData()
+        } else {
+            setLoading(false)
+        }
+    }, [])
     useEffect(() => {
         if (error !== null) {
             toast(error)
@@ -89,8 +141,10 @@ const AuthProvider = ({ children }) => {
         }
     }, [error])
     return (
-        <AuthContext.Provider value={{ signUp, currentUser, signIn }}>
-            {children}
+        <AuthContext.Provider
+            value={{ signUp, currentUser, logIn, logOut, updateUserData }}
+        >
+            {!isLoading ? children : 'Loading...'}
         </AuthContext.Provider>
     )
 }
